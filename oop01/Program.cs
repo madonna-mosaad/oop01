d81612bf55e2acf90cb435825f5335460a123f1f
madonna-mosaad <madonnamosaad86@gmail.com>
2024-07-05 23:10:34 +0300

    question 1

--- oop01/Program.cs
+++ oop01/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace oop01
 {
+    //q1
+    enum Days
+    {
+        Saturday, Sunday, Monday, Tuesday, Wednesday, Thursday, Friday
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Consol
+            #region q1
+            //for (int i = 0; i < 7; i++)
+            //{
+            //    Console.WriteLine((Days)i);
+            //}
+            #endregion
         }
     }
 }
